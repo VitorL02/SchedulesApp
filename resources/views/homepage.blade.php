a5aecdb6cbe80bda22657f1d8af0ef16f745d52a
Vitor Lucas Crispim <vitor.lucas@nscreen.com.br>
2022-07-19 20:01:07 -0300

    Criando a home page
    Foi criado o controller e uma migration para criar no banco as tabelas de cada agendamento, a view da home page será controlada via controller para conseguir receber os dados do agendamento

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,6 +3,26 @@
 
 
 @section('content')
-
-    <h1>Hello Word</h1>
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um Horario</h1>
+        <form action="/" method="get">
+            <input type="datetime-local" name="search" id="search" class="form-control" placeholder="Encontre Seu Horario">
+        </form>
+    </div>
+    <div id="schedules-container">
+        <h2>Horario de Busca:</h2>
+        <p class="subtitle">Proximos Agendamentos</p>
+            <div id="card-container" class="row">
+                <div class="card col-md-3">
+                <ion-icon name="time-outline" class='clock-icon'></ion-icon>
+                <div class="card-body">
+                    <p class="card-date">Data do Agendamento</p>
+                    <h5 class="card-title">Nome:</h5>
+                    <h5 class="card-service">Serviço:</h5>
+                    <p class="card-hour">Horario:</p>
+                    <a href="" class="btn btn-primary" class="edit-button">Editar</a>
+                </div>
+                </div>
+            </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\ScheduleController;
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/',[ScheduleController::class,'index']);
